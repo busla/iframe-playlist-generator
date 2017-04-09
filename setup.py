c29fe89b28bfa9d2929363c761702911f06861f7
Jón Levy <nonni@nonni.cc>
2017-04-09 00:22:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,5 @@ setup(
     packages=['iframeplaylistgenerator'],
     url='https://github.com/pbs/iframe-playlist-generator',
     description='HLS I-frame playlist generator',
-    long_description=open('README.rst').read(),
-    install_requires=['m3u8==0.1.7b'],
-    dependency_links=['https://github.com/pbs/m3u8/tarball/master#egg=m3u8-0.1.7b'],
+    long_description=open('README.rst').read()
 )
